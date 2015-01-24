9b38fa0adb4d90b507d503fb58e1aa063fda1267
Thomas Nattestad <thomas.nattestad@gmail.com>
2015-01-23 23:48:47 -0500

    preliminary

---
+++ snickerdoodle.py
@@ -0,0 +1,9 @@
+import time
+#spotify shit
+dummy=rawinput('start?: ')
+start=time.clock()
+for num in range(60):
+    #dummy spotify play from playlist
+    #dummy display number
+    #dummy beer count
+
