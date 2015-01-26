ae04d4c673d2f6148e15bd1fa5772f0ac8c1b0be
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2015-01-25 22:35:02 -0500

    Functional MVP

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # snickerdoodle
 A Python Spotify Power Hour App
+
+Make sure you add a SPotify API key named "spotify_appkey.key" to the top directory, then simply run snikcerdodole.py with Python.
--- snickerdoodle.py
+++ snickerdoodle.py
@@ -1,9 +1,33 @@
+import spotify
 import time
-#spotify shit
-dummy=rawinput('start?: ')
-start=time.clock()
-for num in range(60):
-    #dummy spotify play from playlist
-    #dummy display number
-    #dummy beer count
-
+import getpass
+session = spotify.Session()
+loop = spotify.EventLoop(session)
+loop.start()
+audio = spotify.PortAudioSink(session)
+User=raw_input('Username: ')
+Pass=getpass.getpass('Password: ')
+session.login(User,Pass)
+time.sleep(3) #TODO: add detection
+start=0
+running=True
+num=0
+print '\nChoose a Playlist:'
+for i in range(len(session.playlist_container)):
+    print str(i+1)+'. '+session.playlist_container[i].name
+print ''
+playlist = session.playlist_container[int(raw_input('Playlist number: '))-1]
+playlist.load()
+tracks = playlist.tracks
+track = tracks[0]
+session.player.load(track)
+dummy=raw_input('Start?')
+while running:
+    if time.time()-start >= 60:
+        num=num+1
+        start=time.time()
+        print str(num)+'. '+str(track.name)
+        session.player.load(track)
+        session.player.play()
+        track = tracks[num]
+        if num==60: running=False
